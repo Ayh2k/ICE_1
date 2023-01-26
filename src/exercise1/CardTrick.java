1c230987e19d563043228acb5ddbbbc916f82261
Ayh2k <112317209+Ayh2k@users.noreply.github.com>
2023-01-25 21:35:35 -0500

    Added information to printInfo()

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -45,7 +45,7 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Ayhan");
+        System.out.println("My name is Ayhan Ahsan");
         System.out.println();
         
         System.out.println("My career ambitions:");
